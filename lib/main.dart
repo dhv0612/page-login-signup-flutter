474198d856cbf3cccc7d0747d4142e4489399a8c
dhv0612 <dhv0612@gmail.com>
2020-11-21 00:35:31 +0700

    login-signup activity

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_signup_page_flutter/constants.dart';
+import 'package:login_signup_page_flutter/Screens/Welcome/welcome_screen.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter login-singin',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
